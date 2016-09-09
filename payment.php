d0e23ec3acc8bb710e00098cdd77eef2f3cdb6d3
Garrett Sand <garrettsand@gmail.com>
2016-09-09 16:08:15 -0700

    Hopefully force https on payment page

--- payment.php
+++ payment.php
@@ -1,5 +1,11 @@
 <?php
 require_once('config.php');
+
+if($_SERVER["HTTPS"] != "on")
+{
+  header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+  exit();
+}
 ?>
 
 <!DOCTYPE html>
