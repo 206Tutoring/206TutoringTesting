bd8d8abe13fd51a54a05fcdd79256e161ad23df8
Garrett Sand <garrettsand@gmail.com>
2016-09-09 16:30:08 -0700

    More HTTPS shenanigans

--- payment.php
+++ payment.php
@@ -1,9 +1,10 @@
 <?php
 require_once('config.php');
 
-(empty($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] !== "on")
-{
-  header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+  $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  header('HTTP/1.1 301 Moved Permanently');
+  header('Location: ' . $redirect);
   exit();
 }
 ?>
