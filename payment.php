b6888d77ef59a0bb62003993dcb2674d9579dbdf
Garrett Sand <garrettsand@gmail.com>
2016-09-09 16:16:46 -0700

    More https garbage

--- payment.php
+++ payment.php
@@ -1,7 +1,7 @@
 <?php
 require_once('config.php');
 
-if($_SERVER["HTTPS"] != "on")
+(empty($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] !== "on")
 {
   header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
   exit();
