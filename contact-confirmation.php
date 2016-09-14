02c94212fed20d9335f3df0342c60260242ebcd9
Garrett Sand <garrettsand@gmail.com>
2016-09-13 17:08:31 -0700

    Split off confirmation page for landing pages

---
+++ contact-confirmation.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Seattle ACT Prep</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../css/small-business.css" rel="stylesheet">
+
+    <!-- Old stylesheets we still need -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
+	  <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
+	  <link rel="stylesheet" type="text/css" href="../slick/slick.css"/>
+	  <link rel="stylesheet" type="text/css" href="../slick/slick-theme.css"/>
+	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top navbar-background" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="../home.php">
+                    <img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
+                </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse text-white" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="../test_prep.php">Test and College Prep</a>
+                    </li>
+                    <li>
+                        <a href="../tutoring.html">Tutoring</a>
+                    </li>
+                      
+                    <li>
+                        <a href="../music.html">Music Lessons</a>
+                    </li>
+
+                    <li>
+                        <a href="../about.php">About</a>
+                    </li>
+
+                    <li>
+                        <a href="../payment.php">Payment</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+        <div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-4">
+              <h1>Thank you for contacting 206 Tutoring! We'll be in touch shortly to talk more about your study goals.</h1>
+              <br>
+              <h1>Best, Chris Morris-Lent</h1>
+              <h1>Director, 206 Tutoring and Music Lessons</h1>
+              <h1>206-551-7843</h1>
+              <!-- Google Code for Converted Form Conversion Page -->
+              <script type="text/javascript">
+              /* <![CDATA[ */
+              var google_conversion_id = 971789382;
+              var google_conversion_language = "en";
+              var google_conversion_format = "3";
+              var google_conversion_color = "ffffff";
+              var google_conversion_label = "oBi8CLeDhmoQxqixzwM";
+              var google_remarketing_only = false;
+              /* ]]> */
+              </script>
+              <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+              </script>
+              <noscript>
+              <div style="display:inline;">
+              <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/971789382/?label=oBi8CLeDhmoQxqixzwM&amp;guid=ON&amp;script=0"/>
+              </div>
+              </noscript>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+              <br>
+            </div>
+            <!-- /.col-md-4 -->
+            <div class="col-md-4">
+            </div>
+        </div>
+        <!-- /.row -->
+        <!-- Footer -->
+        <footer class="col-md-12">
+          <section class="row">
+            <article class="footer-logo pull first">
+              <h1>206 <span class="subtext-dark">tutoring</span></h1>
+              <h3>&copy;206 Tutoring</h3>
+            </article>
+            <article class="footer-contact push info-megablock">
+              <h3>(206) 551-7843</h3>
+              <h3>contact@206tutoring.com</h3>
+            </article>
+          </section>
+        </footer>
+      </div>
+    	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>		
+	<script type="text/javascript">
+	$(function() {
+		// Insert Responsive Sidebar Icon
+		$('<div class="responsive-nav-icon" />').appendTo('.top-nav.row');
+		$('<div class="responsive-nav-close" />').appendTo('nav');
+
+		// Navigation Slide In
+		$('.responsive-nav-icon').click(function() {
+			$('nav').addClass('slide-in');
+			$('html').css("overflow", "hidden");
+			$('#overlay').show();
+			return false;
+		});
+
+		// Navigation Slide Out
+		$('#overlay, .responsive-nav-close').click(function() {
+			$('nav').removeClass('slide-in');
+			$('html').css("overflow", "auto");
+			$('#overlay').hide();
+			return false;
+		});
+	});
+	</script>
+
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
--- landingpage-contactform.php
+++ landingpage-contactform.php
@@ -39,30 +39,9 @@ else {
       ->setText($message . " " . $phonenumber);
     $sendgrid->send($mail);
     ?>
-
-    <h1>Thank you for contacting 206 Tutoring! We'll be in touch shortly to talk more about your study goals.</h1>
-    <br>
-    <h1>Best, Chris Morris-Lent</h1>
-    <h1>Director, 206 Tutoring and Music Lessons</h1>
-    <h1>206-551-7843</h1>
-    <!-- Google Code for Converted Form Conversion Page -->
     <script type="text/javascript">
-    /* <![CDATA[ */
-    var google_conversion_id = 971789382;
-    var google_conversion_language = "en";
-    var google_conversion_format = "3";
-    var google_conversion_color = "ffffff";
-    var google_conversion_label = "oBi8CLeDhmoQxqixzwM";
-    var google_remarketing_only = false;
-    /* ]]> */
+    window.location = "/contact-confirmation.php";
     </script>
-    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-    </script>
-    <noscript>
-    <div style="display:inline;">
-    <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/971789382/?label=oBi8CLeDhmoQxqixzwM&amp;guid=ON&amp;script=0"/>
-    </div>
-    </noscript>
     <?php
   }
 }
