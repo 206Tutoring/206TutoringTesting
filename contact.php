ddbecc55d5f0ac3701dd6b9cbed4515497d2caaf
katiegreentangerine <katie@greentangerineconsulting.com>
2016-11-06 16:13:53 -0800

    Add files via upload

--- contact.php
+++ contact.php
@@ -82,6 +82,56 @@ else                /* send the submitted data */
     }  
 ?>
 						
+<section class="about-content half-row">
+		<a name="contact-form"></a>
+		<article class="about-article get-intouch">
+			<h1>Get in touch</h1>
+			<?php
+      require 'vendor/autoload.php';
+
+      $action=$_REQUEST['action'];
+      if ($action=="")    /* display the contact form */
+          {
+          ?>
+          <form class="lp-contact-form"  action="" method="POST" enctype="multipart/form-data">
+          <input type="hidden" name="action" value="submit">
+          Your name:<br>
+          <input name="name" type="text" value="" size="30"/><br>
+          Your email:<br>
+          <input name="email" type="text" value="" size="30"/><br>
+          Your phone number:<br>
+          <input name="phonenumber" type="text" value="" size="30"/><br>
+          How can we help you?:<br>
+          <textarea name="message" rows="7" cols="30"></textarea><br>
+          <input type="submit" value="Send email"/>
+          </form>
+          <?php
+          } 
+      else {
+        $name=$_REQUEST['name'];
+        $email=$_REQUEST['email'];
+        $phonenumber=$_REQUEST['phonenumber'];
+        $message=$_REQUEST['message'];
+
+        if (($name=="")||($email=="")||($phonenumber=="")||($message=="")) {
+          echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        } else {		
+          $from="From: $name<$email>\r\nReturn-path: $email";
+            $subject="Message sent using your landing page contact form";
+          $sendgrid = new SendGrid($_ENV['SENDGRID_USERNAME'], $_ENV['SENDGRID_PASSWORD']);
+
+          $mail = new SendGrid\Email();
+          $mail->addTo('itscml@gmail.com')
+            ->setFrom($email)
+            ->setSubject('Sent from about page contact form')
+            ->setText($message . " " . $phonenumber);
+          $sendgrid->send($mail);
+          ?>
+          <p>Thank you for your message!</p>
+          <?php
+        }
+      }
+      ?>
 
 
 </body>
