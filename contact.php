eb286a869ca3af0b6c702f1cb0821c9e977c0fec
katiegreentangerine <katie@greentangerineconsulting.com>
2016-11-06 16:08:39 -0800

    Add files via upload

--- contact.php
+++ contact.php
@@ -76,7 +76,7 @@ else                /* send the submitted data */
     else{		
 	    $from="From: $name<$email>\r\nReturn-path: $email";
         $subject="Message sent using your contact form";
-		mail("youremail@yoursite.com", $subject, $message, $from);
+		mail("angelwood@me.com", $subject, $message, $from);
 		echo "Email sent!";
 	    }
     }  
