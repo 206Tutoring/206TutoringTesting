f9917f387b4af4d6430b751d1c1fab0211646f30
katiegreentangerine <katie@greentangerineconsulting.com>
2016-11-13 15:47:23 -0800

    Changed comment

--- home.php
+++ home.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<!-- TODO: Change routes to correct location when in production -->
+	<!-- TODO: This is a test -->
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<meta name="publisher" content="James Hoover"/>
