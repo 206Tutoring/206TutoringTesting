ff7d71bef8c6a9c9f49743a49109fbf9dca4bdb3
katiegreentangerine <katie@greentangerineconsulting.com>
2016-11-15 19:04:05 -0800

    Add files via upload

--- contact.php
+++ contact.php
@@ -33,13 +33,26 @@
 	
 			<article class="address2">
 				<h2 class="prominent">206 Tutoring and Music Lessons</h2>
+			</br>
+			</br>
+			</br>
 				<h2>1512 Summit Ave #304</h2>
 				<h2>Seattle, WA 98122</h2	>
 				<h2>(206) 551-7843</h2	>
 			</article>
-<a href="https://www.google.com/maps/place/3920+Stone+Way+N+%23409,+Seattle,+WA+98103/@47.6547819,-122.3490841,15z/data=!4m5!3m4!1s0x5490145562eefa41:0x217dff24f3468d93!8m2!3d47.654794!4d-122.342066">			
-<img class="center-block" src="/assets/contact/map.jpg" alt="find us on the map">
+<a href="https://www.google.com/maps/place/1512+Summit+Ave+%23304,+Seattle,+WA+98122/@47.6145694,-122.3266819,16z/data=!4m5!3m4!1s0x54906acb14d17daf:0xd0c1ef34305fd942!8m2!3d47.6145658!4d-122.3252174">			
+<img class="center-block" src="/assets/contact/map1.jpg" alt="find us on the map">
 		</a>
+					<article class="address2">
+						<h2>7711 14th Ave NE</h2>
+						<h2>Seattle, WA 98115</h2	>
+						<h2>(206) 551-7843</h2	>
+					</article>
+		<a href="https://www.google.com/maps/place/7711+14th+Avenue+Northeast,+Seattle,+WA+98115/@47.6853611,-122.3180225,16z/data=!4m5!3m4!1s0x54901474d38f0fb1:0xce4aa5bbb60fb473!8m2!3d47.6853575!4d-122.3136398">			
+		<img class="center-block" src="/assets/contact/map2.jpg" alt="find us on the map">
+				</a>
+		
+		
 		</article>
 
 
