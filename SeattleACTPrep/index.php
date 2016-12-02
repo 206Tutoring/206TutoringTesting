fca4336c07d908d0f81b30dbe5c0dd17e6c22e78
katiegreentangerine <katie@greentangerineconsulting.com>
2016-12-01 22:21:14 -0800

    Add files via upload

--- SeattleACTPrep/index.php
+++ SeattleACTPrep/index.php
@@ -1,64 +1,42 @@
-<?php
-session_start();
-$_SESSION['myValue']="Seattle ACT Prep";
-?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../stylesheets/landing-page-style.css">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Seattle ACT Prep</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="../css/small-business.css" rel="stylesheet">
-
-    <!-- Old stylesheets we still need -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
-	  <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
-	  <link rel="stylesheet" type="text/css" href="../slick/slick.css"/>
-	  <link rel="stylesheet" type="text/css" href="../slick/slick-theme.css"/>
-	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
-
 <body>
+	
+	<nav class="navbar navbar-default navbar-fixed-top">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+			<a class="navbar-brand" href="http://www.206tutoring.com"><img class="nav-image" src="../assets/home_page/spaceneedlelogo.png" alt="SpaceNeedle"></a>
+			
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <!--<a class="navbar-brand" href="http://www.206tutoring.com">
+	<img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
+	</a>-->
+	    </div>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top navbar-background" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="../home.php">
-                    <img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
-                </a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse text-white" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li>
                         <a href="../test_prep.php">Test and College Prep</a>
                     </li>
                     <li>
@@ -76,79 +54,48 @@ $_SESSION['myValue']="Seattle ACT Prep";
                     <li>
                         <a href="../payment.php">Payment</a>
                     </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
 
-    <!-- Page Content -->
-        <div>
-            <div class="col-md-4">
-              <img src="../SeattleACTPrep/seattleactprep.png" class="img-responsive" alt="Responsive image">
-            </div>
-            <div class="col-md-4">
-              <h1>SEATTLE ACT PREP</h1>
-              <h6>206 Tutoring offers full-service ACT preparation, with expert instructors and average gains of <b>4 points.</b></h6>
-              <h6>The ACT is more straightforward than the SAT, but its time constraints are difficult and can result in surprise and stress without test preparation. Our ACT preparation focuses on <b>answering questions efficiently.</b></h6>
-              <h6><b>English</b> consists of five passages of fifteen questions each, with an emphasis on grammar and sentence correction.</h6>
-              <h6><b>Math</b> covers more concepts than SAT math, all the way through trigonometry, which means it’s easier—as long as you know math.</h6>
-              <h6><b>Reading</b> contains four passages (prose fiction, humanities, social science, and natural science) with ten questions each.</h6>
-              <h6><b>Science</b> tests the ability to interpret lab reports and graphs. It is similar to the reading section. You don’t even need to know science.</h6>
-            </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-              <?php include '../landingpage-contactform.php'; ?>
-              <br>
-            </div>
-        </div>
-        <!-- /.row -->
-        <!-- Footer -->
-        <footer class="col-md-12">
-          <section class="row">
-            <article class="footer-logo pull first">
-              <h1>206 <span class="subtext-dark">tutoring</span></h1>
-              <h3>&copy;206 Tutoring</h3>
-            </article>
-            <article class="footer-contact push info-megablock">
-              <h3>(206) 551-7843</h3>
-              <h3>contact@206tutoring.com</h3>
-            </article>
-          </section>
-        </footer>
-      </div>
-    	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>		
-	<script type="text/javascript">
-	$(function() {
-		// Insert Responsive Sidebar Icon
-		$('<div class="responsive-nav-icon" />').appendTo('.top-nav.row');
-		$('<div class="responsive-nav-close" />').appendTo('nav');
+	          </ul>
+	        </li>
+	      </ul>
+	      
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="../contact.php">Contact Us</a></li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
 
-		// Navigation Slide In
-		$('.responsive-nav-icon').click(function() {
-			$('nav').addClass('slide-in');
-			$('html').css("overflow", "hidden");
-			$('#overlay').show();
-			return false;
-		});
+<div class="container">
+  <div>
+  		 <h1>SEATTLE ACT PREP</h1>
+		 
+              <div class="col-md-4 landing">
+                <img src="../SeattleACTPrep/cml-pic.jpg" class="img-responsive" alt="Responsive image">
+              </div>
+              <div class="col-md-4 landing-text">
+                <p>206 Tutoring offers full-service ACT preparation, with expert instructors and average gains of <b>4 points.</b></p>
+                <p>The ACT is more straightforward than the SAT, but its time constraints are difficult and can result in surprise and stress without test preparation. Our ACT preparation focuses on <b>answering questions efficiently.</b></p>
+                <p><b>English</b> consists of five passages of fifteen questions each, with an emphasis on grammar and sentence correction.</p>
+                <p><b>Math</b> covers more concepts than SAT math, all the way through trigonometry, which means it’s easier—as long as you know math.</p>
+                <p><b>Reading</b> contains four passages (prose fiction, humanities, social science, and natural science) with ten questions each.</p>
+                <p><b>Science</b> tests the ability to interpret lab reports and graphs. It is similar to the reading section. You don’t even need to know science.<p>
+              </div>
+			  <div class="col-md-4">
+			                <?php include '../landingpage-contactform.php'; ?>
+			                <br>
+			              </div>
+              
+  		
+          </div>
 
-		// Navigation Slide Out
-		$('#overlay, .responsive-nav-close').click(function() {
-			$('nav').removeClass('slide-in');
-			$('html').css("overflow", "auto");
-			$('#overlay').hide();
-			return false;
-		});
-	});
-	</script>
-
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
+  
+</div>
+<!-- Footer -->
+<?php include "../footer.php"; ?>
 </body>
+</html>
+
+
+
+
