f7da88b4c8e74e200cdc89ac802befddd1811171
katiegreentangerine <katie@greentangerineconsulting.com>
2016-12-02 12:41:12 -0800

    Add files via upload

--- SeattleACTPrep/index.php
+++ SeattleACTPrep/index.php
@@ -66,51 +66,42 @@
 	  </div><!-- /.container-fluid -->
 	</nav>
 
-<div class="container">
-  <div>
-  		 <h1>SEATTLE ACT PREP</h1>
-		 
-              <div class="col-md-4 one-third first landing">
-                <img src="../SeattleACTPrep/cml-pic.jpg" class="img-responsive" alt="Responsive image">
-              </div>
-              <div class="col-md-4 one-third landing-text">
-                <p>206 Tutoring offers full-service ACT preparation, with expert instructors and average gains of <b>4 points.</b></p>
-                <p>The ACT is more straightforward than the SAT, but its time constraints are difficult and can result in surprise and stress without test preparation. Our ACT preparation focuses on <b>answering questions efficiently.</b></p>
-                <p><b>English</b> consists of five passages of fifteen questions each, with an emphasis on grammar and sentence correction.</p>
-                <p><b>Math</b> covers more concepts than SAT math, all the way through trigonometry, which means it’s easier—as long as you know math.</p>
-                <p><b>Reading</b> contains four passages (prose fiction, humanities, social science, and natural science) with ten questions each.</p>
-                <p><b>Science</b> tests the ability to interpret lab reports and graphs. It is similar to the reading section. You don’t even need to know science.<p>
-              </div>
-			  <div class="col-md-4 one-third">
-			                <?php include '../landingpage-contactform.php'; ?>
-			                <br>
-			              </div>
-              
-  		
-          </div>
 
-  
+
+<div class="container">
+	 <h1>SEATTLE ACT PREP</h1>
+  <div class="row">
+    <div class="col-sm-4">
+       <img src="../SeattleACTPrep/cml-pic.jpg" class="img-responsive" alt="Responsive image">
+    </div>
+    <div class="col-sm-4 landing-text">
+      <p>206 Tutoring offers full-service ACT preparation, with expert instructors and average gains of <b>4 points.</b></p>
+      <p>The ACT is more straightforward than the SAT, but its time constraints are difficult and can result in surprise and stress without test preparation. Our ACT preparation focuses on <b>answering questions efficiently.</b></p>
+      <p><b>English</b> consists of five passages of fifteen questions each, with an emphasis on grammar and sentence correction.</p>
+      <p><b>Math</b> covers more concepts than SAT math, all the way through trigonometry, which means it’s easier—as long as you know math.</p>
+      <p><b>Reading</b> contains four passages (prose fiction, humanities, social science, and natural science) with ten questions each.</p>
+      <p><b>Science</b> tests the ability to interpret lab reports and graphs. It is similar to the reading section. You don’t even need to know science.<p>
+    </div>
+    <div class="col-sm-4">
+      <?php include '../landingpage-contactform.php'; ?> 
+    </div>
+  </div>
 </div>
 
 
+
 <footer>
-  <section class="row">
-    <article class="footer-logo pull first">
-      <h1>206 <span class="subtext-dark">tutoring</span></h1>
-      <h3>&copy;206 Tutoring</h3>
-    </article>
-    <article class="footer-contact push info-megablock">
-      <h3>(206) 551-7843</h3>
-	</br>
-      <h3>contact@206tutoring.com</h3>
-    </article>
-  </section>
+  
+      <h1>206 <span class="subtext-dark">Tutoring</span> </h1>
+	<div class="footer-text">	
+		 <p> &copy;206 Tutoring &bull; contact@206tutoring.com &bull; (206) 551-7843 </p>
+  </div>
+    
 </footer>
 
 
 
-<!-- Footer -->
-<?php include "../footer.php"; ?>
+
 </body>
 </html>
 
