f68cbd32699a8fd03cda9ce44f71fc8fd27176e4
katiegreentangerine <katie@greentangerineconsulting.com>
2016-12-17 09:16:51 -0800

    Add files via upload

--- SeattleMathTutoring/index.php
+++ SeattleMathTutoring/index.php
@@ -1,71 +1,50 @@
-<?php
-session_start();
-$_SESSION['myValue']="Seattle Math Tutoring";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+  <title>206 Tutoring - Seattle ACT Prep</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../stylesheets/landing-page-style.css">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Seattle Math Tutoring</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="../css/small-business.css" rel="stylesheet">
-
-    <!-- Old stylesheets we still need -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
-	  <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
-	  <link rel="stylesheet" type="text/css" href="../slick/slick.css"/>
-	  <link rel="stylesheet" type="text/css" href="../slick/slick-theme.css"/>
-	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
-
 <body>
+	
+	
+	
+	<nav class="navbar navbar-default navbar-fixed-top center">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+			<a class="navbar-brand" href="http://www.206tutoring.com"><img class="nav-image" src="../assets/home_page/spaceneedlelogo.png" alt="SpaceNeedle"></a>
+			
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <!--<a class="navbar-brand" href="http://www.206tutoring.com">
+	<img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
+	</a>-->
+	    </div>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top navbar-background" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="../home.php">
-                    <img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
-                </a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse text-white" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li>
                         <a href="../test_prep.php">Test and College Prep</a>
                     </li>
                     <li>
                         <a href="../tutoring.html">Tutoring</a>
                     </li>
-                      
+                      <li><a href="/groupclasses.html" class="nav-link">Group Classes</a></li>
                     <li>
                         <a href="../music.html">Music Lessons</a>
                     </li>
@@ -73,84 +52,79 @@ $_SESSION['myValue']="Seattle Math Tutoring";
                     <li>
                         <a href="../about.php">About</a>
                     </li>
-
+                    <li>
+                        <a href="../instructors.html">Instructors</a>
+                    </li>
                     <li>
                         <a href="../payment.php">Payment</a>
                     </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+					
+					
 
-    <!-- Page Content -->
-        </div>
-        <!-- /.row -->
+	          </ul>
+	        </li>
+	      </ul>
+		  <ul class="nav navbar-right">
+		                      <li><a href="../contact.php" class="btn btn-orange btn-contact">Contact Us</a></li>
+		                    </ul>
+	     
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
 
-        <!-- Content Row -->
-        <div>
-            <div class="col-md-4">
-              <img src="../SeattleMathTutoring/seattlemathtutoring.png" class="img-responsive" alt="Responsive image">
-            </div>
-            <div class="col-md-4">
-              <h1>SEATTLE MATH TUTORING</h1>
-              <h6>At 206 Tutoring, we believe that <b>every student deserves to understand math.</b> Math shouldn’t be a cause for anxiety and frustration, but <b>confidence and discovery.</b></h6>
-              <h6>Our tutors have experience teaching everything from basic addition to calculus, ages 5 to 45. Several of us were mathletes in middle school, and were two to three years ahead in math in high school. We earned <b>4’s or 5’s</b> on the <b>Calculus AB, Calculus BC, and Statistics AP tests.</b> Our SAT math scores range from <b>750 to 800.</b></h6>
-              <h6>Every math class could be aced by a majority of students. Yet math is the subject taught worst in school. New teaching methods force students to adopt methods that don’t fit their style. Often, explanations confuse instead of clarifying. Our teaching reverses this. Our <b>individualized lessons</b> adapt to each student’s learning style. There may only be one right answer, but there are countless ways to get that answer.</h6>
-            </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-              <?php include '../landingpage-contactform.php'; ?>
-              <br>
-            </div>
-        </div>
-        <!-- /.row -->
-        <!-- Footer -->
-        <footer class="col-md-12">
-          <section class="row">
-            <article class="footer-logo pull first">
-              <h1>206 <span class="subtext-dark">tutoring</span></h1>
-              <h3>&copy;206 Tutoring</h3>
-            </article>
-            <article class="footer-contact push info-megablock">
-              <h3>(206) 551-7843</h3>
-              <h3>contact@206tutoring.com</h3>
-            </article>
-          </section>
-        </footer>
-      </div>
-    	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>		
-	<script type="text/javascript">
-	$(function() {
-		// Insert Responsive Sidebar Icon
-		$('<div class="responsive-nav-icon" />').appendTo('.top-nav.row');
-		$('<div class="responsive-nav-close" />').appendTo('nav');
 
-		// Navigation Slide In
-		$('.responsive-nav-icon').click(function() {
-			$('nav').addClass('slide-in');
-			$('html').css("overflow", "hidden");
-			$('#overlay').show();
-			return false;
-		});
 
-		// Navigation Slide Out
-		$('#overlay, .responsive-nav-close').click(function() {
-			$('nav').removeClass('slide-in');
-			$('html').css("overflow", "auto");
-			$('#overlay').hide();
-			return false;
-		});
-	});
-	</script>
 
-    <!-- /.container -->
+<div class="container">
+	 <h1 class="top-header">SEATTLE'S BEST MATH TUTORS</h1>
+  <div class="row">
+	  
+      <div class="landing-content col-sm-4">
+        <p>206 Tutoring offers individualized, results-focused math instruction with the greatest improvements in the Seattle area. Raise your test scores by <strong>one full letter grade</strong>, your SAT score by <strong>400</strong>, your ACT score by<strong> 4</strong>, or your conceptual understanding by <strong>one whole year</strong>.
+</p>
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+<p>We teach everything from basic addition to calculus, ages 5 to 65. We earned <strong>4’s or 5’s</strong> on the <strong>math AP tests</strong>. Our SAT math scores range from <strong>700 to 800</strong>.</p>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+        <p> Our <strong>individualized lessons</strong> adapt to each student’s learning style and goals. Meet <strong>in your home</strong> or at the <strong>location of your choice</strong>, across <strong>the entire Seattle area</strong>.</p>
+              </div>
+	  
+        
+    <div class="landing-content col-sm-4">
+      <?php include '../landingpage-contactform.php'; ?>
+	
+    </div>
+
+<div class="landing-content col-sm-4">
+       <img src="../assets/landingpages/cml-pic.jpg" class="img-responsive" alt="Responsive image">
+	   <section>
+	   <p>
+	   	"Mr. Morris-Lent recognizes the importance of his role as a “guiding force” in influencing the educational endeavors of his students. He respects parents’ impelling positive impact in the educational voyage of their children … goals not based on dictated tenets and past performances, but toward desired results." <i> - Siri H., Redmond</i>
+		
+	   </p>
+   </section>
+    </div>
+
+
+  </div>
+</div>
+
+
+
+<footer>
+  
+      <h1 class="footer-logo">206 <span class="subtext-dark">Tutoring</span> </h1>
+	<div class="footer-text">	
+		 <p> &copy;206 Tutoring &bull; contact@206tutoring.com &bull; (206) 551-7843 </p>
+  </div>
+    
+</footer>
+
+
+
 
 </body>
+</html>
+
+
+
+
