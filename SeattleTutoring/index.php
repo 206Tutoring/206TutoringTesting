40321d1087da28749da972f0f07838b9827efce1
katiegreentangerine <katie@greentangerineconsulting.com>
2016-12-17 09:18:49 -0800

    Add files via upload

--- SeattleTutoring/index.php
+++ SeattleTutoring/index.php
@@ -1,71 +1,50 @@
-<?php
-session_start();
-$_SESSION['myValue']="Seattle Tutoring";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+  <title>206 Tutoring - Seattle ACT Prep</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../stylesheets/landing-page-style.css">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Seattle Tutoring</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="../css/small-business.css" rel="stylesheet">
-
-    <!-- Old stylesheets we still need -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
-	  <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
-	  <link rel="stylesheet" type="text/css" href="../slick/slick.css"/>
-	  <link rel="stylesheet" type="text/css" href="../slick/slick-theme.css"/>
-	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
-
 <body>
+	
+	
+	
+	<nav class="navbar navbar-default navbar-fixed-top center">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+			<a class="navbar-brand" href="http://www.206tutoring.com"><img class="nav-image" src="../assets/home_page/spaceneedlelogo.png" alt="SpaceNeedle"></a>
+			
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <!--<a class="navbar-brand" href="http://www.206tutoring.com">
+	<img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
+	</a>-->
+	    </div>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top navbar-background" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="../home.php">
-                    <img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
-                </a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse text-white" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li>
                         <a href="../test_prep.php">Test and College Prep</a>
                     </li>
                     <li>
                         <a href="../tutoring.html">Tutoring</a>
                     </li>
-                      
+                      <li><a href="/groupclasses.html" class="nav-link">Group Classes</a></li>
                     <li>
                         <a href="../music.html">Music Lessons</a>
                     </li>
@@ -73,79 +52,80 @@ $_SESSION['myValue']="Seattle Tutoring";
                     <li>
                         <a href="../about.php">About</a>
                     </li>
-
+                    <li>
+                        <a href="../instructors.html">Instructors</a>
+                    </li>
                     <li>
                         <a href="../payment.php">Payment</a>
                     </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+					
+					
 
-    <!-- Page Content -->
-        <div>
-            <div class="col-md-4">
-              <img src="../SeattleTutoring/seattletutoring.png" class="img-responsive" alt="Responsive image">
-            </div>
-            <div class="col-md-4">
-              <h1>SEATTLE TUTORING</h1>
-              <h6>At 206 Tutoring, our team of tutors gives lessons in math, reading and writing, and more. But what we’re really teaching our students is how to <b>deal with school:</b> finding out what teachers really want, discovering the discipline to study before a big test, and getting the most out of every lecture and paper. <b>206 Tutoring specializes in helping students learn how to learn.</b></h6>
-              <h6>When students learn how to learn, they learn not just one subject, but all of them. They become their own teachers, able to judge a context or situation for themselves. They develop their own senses of style and methods for accomplishing their objectives. They free up time and energy to devote to the things that interest them—which are also easier to master, when they’ve learned how to learn.</h6>
-            </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-              <?php include '../landingpage-contactform.php'; ?>
-              <br>
-            </div>
-        </div>
-        <!-- /.row -->
-        <!-- Footer -->
-        <footer class="col-md-12">
-          <section class="row">
-            <article class="footer-logo pull first">
-              <h1>206 <span class="subtext-dark">tutoring</span></h1>
-              <h3>&copy;206 Tutoring</h3>
-            </article>
-            <article class="footer-contact push info-megablock">
-              <h3>(206) 551-7843</h3>
-              <h3>contact@206tutoring.com</h3>
-            </article>
-          </section>
-        </footer>
-      </div>
-    	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>		
-	<script type="text/javascript">
-	$(function() {
-		// Insert Responsive Sidebar Icon
-		$('<div class="responsive-nav-icon" />').appendTo('.top-nav.row');
-		$('<div class="responsive-nav-close" />').appendTo('nav');
+	          </ul>
+	        </li>
+	      </ul>
+		  <ul class="nav navbar-right">
+		                      <li><a href="../contact.php" class="btn btn-orange btn-contact">Contact Us</a></li>
+		                    </ul>
+	     
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
 
-		// Navigation Slide In
-		$('.responsive-nav-icon').click(function() {
-			$('nav').addClass('slide-in');
-			$('html').css("overflow", "hidden");
-			$('#overlay').show();
-			return false;
-		});
 
-		// Navigation Slide Out
-		$('#overlay, .responsive-nav-close').click(function() {
-			$('nav').removeClass('slide-in');
-			$('html').css("overflow", "auto");
-			$('#overlay').hide();
-			return false;
-		});
-	});
-	</script>
 
-    <!-- /.container -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+<div class="container">
+	 <h1 class="top-header">SEATTLE'S BEST TUTORS</h1>
+  <div class="row">
+	  
+      <div class="landing-content col-sm-4">
+        <p>206 Tutoring offers elite, results-focused academic instruction from experienced tutors at the location of your choice. We give lessons in<strong>math, reading</strong> and <strong>writing</strong>, and more.
+</p>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+<p>All of our instructors have scored at least 2100 on the <strong>SAT</strong>, 31 on the <strong>ACT</strong>, and 330 on the <strong>GRE</strong>. Our average improvement is <strong>300 points</strong>on the SAT, <strong>4 points</strong> on the ACT, and <strong>7 points</strong> on the GRE.
+</p>
+
+        <p><strong>We’ll help you learn how to learn. Contact us today.</strong></p>
+              </div>
+	  
+        
+    <div class="landing-content col-sm-4">
+      <?php include '../landingpage-contactform.php'; ?>
+	
+    </div>
+
+<div class="landing-content col-sm-4">
+       <img src="../assets/landingpages/cml-pic.jpg" class="img-responsive" alt="Responsive image">
+	   <section>
+	   <p>
+	   	"Mr. Morris-Lent recognizes the importance of his role as a “guiding force” in influencing the educational endeavors of his students. He respects parents’ impelling positive impact in the educational voyage of their children … goals not based on dictated tenets and past performances, but toward desired results." <i> - Siri H., Redmond</i>
+		
+	   </p>
+   </section>
+    </div>
+
+
+  </div>
+</div>
+
+
+
+<footer>
+  
+      <h1 class="footer-logo">206 <span class="subtext-dark">Tutoring</span> </h1>
+	<div class="footer-text">	
+		 <p> &copy;206 Tutoring &bull; contact@206tutoring.com &bull; (206) 551-7843 </p>
+  </div>
+    
+</footer>
+
+
+
 
 </body>
+</html>
+
+
+
+
