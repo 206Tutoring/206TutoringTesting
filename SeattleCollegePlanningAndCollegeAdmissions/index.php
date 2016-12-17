24d5e604fa45c6d26fb7d81dc86fdf7bd41db9f0
katiegreentangerine <katie@greentangerineconsulting.com>
2016-12-17 09:15:04 -0800

    Add files via upload

--- SeattleCollegePlanningAndCollegeAdmissions/index.php
+++ SeattleCollegePlanningAndCollegeAdmissions/index.php
@@ -1,70 +1,50 @@
-<?php
-session_start();
-$_SESSION['myValue']="Seattle College Planning and College Admissions";
-?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
+  <title>206 Tutoring - Seattle ACT Prep</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="../stylesheets/landing-page-style.css">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Seattle College Planning and College Admissions</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="../css/small-business.css" rel="stylesheet">
-
-    <!-- Old stylesheets we still need -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
-	  <link rel="stylesheet" type="text/css" href="../stylesheets/style.css">
-	  <link rel="stylesheet" type="text/css" href="../slick/slick.css"/>
-	  <link rel="stylesheet" type="text/css" href="../slick/slick-theme.css"/>
-	  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-	
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
-
 <body>
+	
+	
+	
+	<nav class="navbar navbar-default navbar-fixed-top center">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+			<a class="navbar-brand" href="http://www.206tutoring.com"><img class="nav-image" src="../assets/home_page/spaceneedlelogo.png" alt="SpaceNeedle"></a>
+			
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <!--<a class="navbar-brand" href="http://www.206tutoring.com">
+	<img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
+	</a>-->
+	    </div>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top navbar-background" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="../home.php">
-                    <img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
-                </a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse text-white" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        <li>
                         <a href="../test_prep.php">Test and College Prep</a>
                     </li>
                     <li>
                         <a href="../tutoring.html">Tutoring</a>
                     </li>
-                      
+                      <li><a href="/groupclasses.html" class="nav-link">Group Classes</a></li>
                     <li>
                         <a href="../music.html">Music Lessons</a>
                     </li>
@@ -72,90 +52,90 @@ $_SESSION['myValue']="Seattle College Planning and College Admissions";
                     <li>
                         <a href="../about.php">About</a>
                     </li>
-
+                    <li>
+                        <a href="../instructors.html">Instructors</a>
+                    </li>
                     <li>
                         <a href="../payment.php">Payment</a>
                     </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+					
+					
 
-    <!-- Page Content -->
-        <div>
-            <div class="col-md-4">
-              <img src="../SeattleCollegePlanningAndCollegeAdmissions/seattlecollegeplanningandcollegeadmissions.png" class="img-responsive" alt="Responsive image">
-            </div>
-            <div class="col-md-4">
-              <h4>SEATTLE COLLEGE PLANNING AND COLLEGE ADMISSIONS</h4>
-              <h6>The college-admissions process is intimidating if you’ve never done it before. We have! Let us help you. 206 Tutoring’s college counseling services offer pragmatic and decisive solutions to an increasingly competitive procedure.</h6>
-              <h6>We will help you:</h6>
-                  <h6>—Identify and apply to a range of colleges you’d be happy to attend</h6>
-                  <h6>—Optimize standardized-test scores and GPAs</h6>
-                  <h6>—Write the best possible college essays</h6>
-                  <h6>—Find and win scholarships and opportunities for financial aid</h6>
-              <h6>We have the following qualifications:</h6>
-                  <h6>—Admission to Ivies and prestigious liberal-arts colleges</h6>
-                  <h6>—Experience with broader demographics than other private agencies</h6>
-                  <h6>—Familiarity with the demands of the modern application process</h6>
-                  <h6>—Youth and experience: as millennials, we know what colleges are like from firsthand experience, not admissions brochures or officers</h6>        
-                  <h6>—Comprehensive knowledge of colleges and how they fit with personality types</h6>
-                  <h6>—Extensive experience editing essays from published writers with exceptional skills</h6>
-            </div>
-            <!-- /.col-md-4 -->
-            <div class="col-md-4">
-              <?php include '../landingpage-contactform.php'; ?>
-              <br>
-            </div>
-        </div>
-        <!-- /.row -->
-        <!-- Footer -->
-        <footer class="col-md-12">
-          <section class="row">
-            <article class="footer-logo pull first">
-              <h1>206 <span class="subtext-dark">tutoring</span></h1>
-              <h3>&copy;206 Tutoring</h3>
-            </article>
-            <article class="footer-contact push info-megablock">
-              <h3>(206) 551-7843</h3>
-              <h3>contact@206tutoring.com</h3>
-            </article>
-          </section>
-        </footer>
-      </div>
-    	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>		
-	<script type="text/javascript">
-	$(function() {
-		// Insert Responsive Sidebar Icon
-		$('<div class="responsive-nav-icon" />').appendTo('.top-nav.row');
-		$('<div class="responsive-nav-close" />').appendTo('nav');
+	          </ul>
+	        </li>
+	      </ul>
+		  <ul class="nav navbar-right">
+		                      <li><a href="../contact.php" class="btn btn-orange btn-contact">Contact Us</a></li>
+		                    </ul>
+	     
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
 
-		// Navigation Slide In
-		$('.responsive-nav-icon').click(function() {
-			$('nav').addClass('slide-in');
-			$('html').css("overflow", "hidden");
-			$('#overlay').show();
-			return false;
-		});
 
-		// Navigation Slide Out
-		$('#overlay, .responsive-nav-close').click(function() {
-			$('nav').removeClass('slide-in');
-			$('html').css("overflow", "auto");
-			$('#overlay').hide();
-			return false;
-		});
-	});
-	</script>
 
-    <!-- /.container -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+<div class="container">
+	 <h1 class="top-header">SEATTLE'S BEST COLLEGE PLANNING AND COLLEGE ADMISSIONS TUTORS</h1>
+  <div class="row">
+	  
+      <div class="landing-content col-sm-4">
+        <p>We Will:
+			<ul>
+				<li>Identify and apply to a range of colleges you’d be happy to attend</li>
+				<li>Optimize standardized-test scores and GPAs</li>
+				<li>Find and win scholarships and opportunities for financial aid</li>
+			</ul>
+					
+</p>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+<p>We have the following qualifications:
+			<ul>
+				<li>Admission to Ivies and prestigious liberal-arts colleges</li>
+				<li>Experience with broader demographics than other private agencies</li>
+				<li>Comprehensive knowledge of colleges and how they fit with personality types</li>
+			</ul></p>
+
+        <p>Get in touch and start planning your journey today!</p>
+              </div>
+	  
+        
+    <div class="landing-content col-sm-4">
+      <?php include '../landingpage-contactform.php'; ?>
+	
+    </div>
+
+<div class="landing-content col-sm-4">
+       <img src="../assets/landingpages/cml-pic.jpg" class="img-responsive" alt="Responsive image">
+	   <section>
+	   <p>
+	   	"Mr. Morris-Lent recognizes the importance of his role as a “guiding force” in influencing the educational endeavors of his students. He respects parents’ impelling positive impact in the educational voyage of their children … goals not based on dictated tenets and past performances, but toward desired results." <i> - Siri H., Redmond</i>
+		
+	   </p>
+   </section>
+    </div>
+
+
+  </div>
+</div>
+
+
+
+<footer>
+  
+      <h1 class="footer-logo">206 <span class="subtext-dark">Tutoring</span> </h1>
+	<div class="footer-text">	
+		 <p> &copy;206 Tutoring &bull; contact@206tutoring.com &bull; (206) 551-7843 </p>
+  </div>
+    
+</footer>
+
+
+
 
 </body>
+</html>
+
+
+
+
