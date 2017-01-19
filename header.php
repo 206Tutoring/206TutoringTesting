98a525d45a8ec1ed6af1ec5dde8be44e5e917a7c
katiegreentangerine <katie@greentangerineconsulting.com>
2017-01-18 20:53:29 -0800

    Add files via upload

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
  	        
 <span class="sr-only">Toggle navigation</span>
-  	       MENU <span class="caret"></span>
+  	       MENU ☰
   	      </button>
 
   	      <!--<a class="navbar-brand" href="http://www.206tutoring.com">
@@ -16,10 +16,11 @@
   	    </div>
 
   	    <!-- Collect the nav links, forms, and other content for toggling -->
-  	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  	    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+
   	      <ul class="nav navbar-nav">
 		<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Types of Tutoring<span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tutoring Services<span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li>
                           <a href="../test_prep.php">Test and College Prep</a>
@@ -49,11 +50,13 @@
                           <a href="../payment.php">Payment</a>
                       </li>
 
+
+
   	          </ul>
   	        </li>
   	      </ul>
-<ul class="nav navbar-right">
-		                      <li><a href="../contact.php" class="btn-orange btn-contact">Contact Us</a></li>
+ <ul class="nav navbar-right">
+		                      <li><a href="../contact.php#contact-form" class="btn-orange btn-contact">Contact Us</a></li>
 		                    </ul>
 	      
 	     
@@ -70,13 +73,7 @@
           <img src="/assets/home_page/spaceneedlelogo.png" alt="SpaceNeedle" class="spaceneedle-logo">
         </a>
       </div>
-      <div class="nav-badge nav-btn one-fifth push">
-         
-        <div class="phone-numb">
-          <p></p>
-        </div>
-      </div>
-      <!-- START NAV -->
+            <!-- START NAV -->
       <nav>
        <!-- <ul class="nav-link-list">
           <li><a href="/test_prep.php" class="nav-link">TEST & COLLEGE PREP</a></li>
@@ -87,16 +84,26 @@
           <li><a href="/payment.php" class="nav-link">PAYMENT</a></li>
         </ul>-->
         <section>
-          <div class="pull">
+          <div class="one-fifth push">
             <a href="http://www.seattletimes.com/education-lab/guest-essay-how-many-good-high-schools-can-be-so-bad/" class="nav-link">
               <img src="/assets/home_page/seattletimes.png" alt="SeattleTimes" class="seattletimes-logo">
-              <br>206 Tutoring in the Seattle Times:<br>How to reduce stress in high school<br>
+              <br>206 Tutoring Featured in the Seattle Times
             </a>
           </div>
-          <div class="push">
+          <div class="one-fifth push">
+              <a href="https://www.thumbtack.com/wa/seattle/test-prep/tutoring-piano-lessons" class="nav-link">
+                <img src="/assets/about/ThumbtackBadge2016.png" alt="Thumbtack" class="thumbtack-logo2016">
+                <br>5 stars on Thumbtack<br>Best of 2016<br>
+              </a>
+            </div>
+
+
+
+<div class="one-fifth push">
+	
             <a href="https://www.thumbtack.com/wa/seattle/test-prep/tutoring-piano-lessons" class="nav-link">
               <img src="/assets/about/ThumbtackBadge.png" alt="Thumbtack" class="thumbtack-logo">
-              <br>5 stars on Thumbtack: <br>Best of 2015<br>
+              <br>5 stars on Thumbtack<br>Best of 2015<br>
             </a>
           </div>
         </section>
@@ -112,7 +119,7 @@
           <div>
             <h2>experienced, effective, empowering</h2>
           </div>
-          <a href="/contact.php" class="btn-orange">Contact Us Now</a>
+         
         </main>
       </span>
     </div>
