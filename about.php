bbff3f0ad48838aa09fe3a73704ae5e89f3f1321
katiegreentangerine <katie@greentangerineconsulting.com>
2017-02-16 09:20:39 -0800

    Add files via upload

--- about.php
+++ about.php
@@ -42,7 +42,7 @@
   	<img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
   	</a>-->
   	    </div>
-
+<a class="navbar-text">Guaranteed Score Boost<br>New SAT +200 ACT +4</a>
   	    <!-- Collect the nav links, forms, and other content for toggling -->
   	    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
   	      <ul class="nav navbar-nav">
