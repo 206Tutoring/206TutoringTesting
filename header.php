6f330107c706ae4568f9d4168327823c5b23ac30
katiegreentangerine <katie@greentangerineconsulting.com>
2017-02-15 17:46:06 -0800

    Added score boosts to nav bar

--- header.php
+++ header.php
@@ -12,11 +12,13 @@
 
   	      
   	    </div>
-
+<a class="navbar-text">Guaranteed Score Boost<br>New SAT +200 ACT +4</a>
   	    <!-- Collect the nav links, forms, and other content for toggling -->
   	    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
 
-  	      <ul class="nav navbar-nav">
+     
+
+ <ul class="nav navbar-nav">
 		<li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tutoring Services<span class="caret"></span></a>
           <ul class="dropdown-menu">
