884d05c1fd79ae2b14c38f69c84fe69ca505710e
katiegreentangerine <katie@greentangerineconsulting.com>
2017-02-18 21:50:38 -0800

    Add files via upload
    Removed badges from page headers

--- test_prep.php
+++ test_prep.php
@@ -89,53 +89,10 @@
     <section class="top-nav row">
       <a href="/home.php" class="nav-logo pull first">
         <img src="/assets/home_page/spaceneedlelogo.png" alt="SpaceNeedle" class="spaceneedle-logo"></a>
-      <div class="nav-badge nav-btn one-fifth push">
-            
-          <div class="phone-numb">
-            <p></p>
-          </div>
-      </div>
-      <!-- START NAV -->
-      <nav>
-      <!--  <ul class="nav-link-list">
-          <li><a href="/test_prep.php" class="nav-link">TEST & COLLEGE PREP</a></li>
-          <li><a href="/tutoring.html" class="nav-link">TUTORING</a></li>
-		  <li><a href="/groupclasses.html" class="nav-link">GROUP CLASSES</a></li>
-          <li><a href="/music.html" class="nav-link">MUSIC LESSONS</a></li>
-          <li><a href="/about.php" class="nav-link">ABOUT</a></li>
-          <li><a href="/payment.php" class="nav-link">PAYMENT</a></li>
-        </ul> -->
-        <section>
-          <div class="one-fifth push">
-            <a href="http://www.seattletimes.com/education-lab/guest-essay-how-many-good-high-schools-can-be-so-bad/" class="nav-link">
-              <img src="/assets/home_page/seattletimes.png" alt="SeattleTimes" class="seattletimes-logo">
-              <br>206 Tutoring Featured in the Seattle Times
-            </a>
-          </div>
-          <div class="one-fifth push">
-              <a href="https://www.thumbtack.com/wa/seattle/test-prep/tutoring-piano-lessons" class="nav-link">
-                <img src="/assets/about/thumbtackbadge2016.png" alt="Thumbtack" class="thumbtack-logo2016">
-                <br>5 stars on Thumbtack<br>Best of 2016<br>
-              </a>
-            </div>
-
-
-
-<div class="one-fifth push">
-	
-            <a href="https://www.thumbtack.com/wa/seattle/test-prep/tutoring-piano-lessons" class="nav-link">
-              <img src="/assets/about/ThumbtackBadge.png" alt="Thumbtack" class="thumbtack-logo">
-              <br>5 stars on Thumbtack<br>Best of 2015<br>
-            </a>
-          </div>
-
-        </section>
-      </nav>
-    </section>
-    <div>
-      <span>
+<span>
         <main class="header-text row">
           <div>
+<br>
             <h1>TEST PREP AND COLLEGE PLANNING</h1>
           </div>
           <br>
@@ -145,7 +102,9 @@
           
         </main>
       </span>
-    </div>
+
+              </section>
+    
   </div>
   </header>
 	<span class="jump-links"><a href="#sat">SAT</a> | <a href="#act">ACT</a> | <a href="#collegePlanning">College Planning</a> | <a href="#other">Other Tests</a></span>
