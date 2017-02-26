8afcb9c373db2fa17b82e182e91813416c43fcd6
katiegreentangerine <katie@greentangerineconsulting.com>
2017-02-26 15:07:27 -0800

    Add files via upload

--- payment.php
+++ payment.php
@@ -65,14 +65,13 @@ require_once('config.php');
 </head>
 <!-- START BODY -->
 <body>
-
-  	<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top">
   	  <div class="container-fluid">
   	    <!-- Brand and toggle get grouped for better mobile display -->
   	    <div class="navbar-header">
 			
 			
-  	      <button type="button" class="navbar-toggle collapsed navbar-right" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+  	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
  	        
 <span class="sr-only">Toggle navigation</span>
   	       MENU ☰
@@ -127,27 +126,23 @@ require_once('config.php');
   	    </div><!-- /.navbar-collapse -->
   	  </div><!-- /.container-fluid -->
   	</nav>
-
-
-
-
   <!-- START HEADER -->
 <header class="home-header master-header payment-header">
   <div class="full-row gradient">
     <section class="top-nav row">
       <a href="/home.php" class="nav-logo pull first">
         <img src="/assets/home_page/spaceneedlelogo.png" alt="SpaceNeedle" class="spaceneedle-logo"></a>
-         </section>
-    <div>
-      <span>
+<span>
         <main class="header-text row">
           <div>
+<br>
             <h1>PAYMENT</h1>
           </div>
         </main>
       </span>
-    </div>
-  </div>
+
+         </section>
+      </div>
 </header>
 
 <body>
