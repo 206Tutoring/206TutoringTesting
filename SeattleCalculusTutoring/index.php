95785e8079aa4d07a536ab8f8192b9e9688b6922
katiegreentangerine <katie@greentangerineconsulting.com>
2017-03-05 12:09:21 -0800

    Add files via upload

--- SeattleCalculusTutoring/index.php
+++ SeattleCalculusTutoring/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>206 Tutoring - Seattle ACT Prep</title>
+  <title>206 Tutoring - Seattle Calculus Tutoring</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="../stylesheets/landing-page-style.css">
 
 
@@ -15,91 +16,60 @@
 
 </head>
 <body>
+
 	
+	<div class=color-bar-header>
+	</div>
 	
-	
-	<nav class="navbar navbar-default navbar-fixed-top center">
-	  <div class="container-fluid">
-	    <!-- Brand and toggle get grouped for better mobile display -->
-	    <div class="navbar-header">
-			<a class="navbar-brand" href="http://www.206tutoring.com"><img class="nav-image" src="../assets/home_page/spaceneedlelogo.png" alt="SpaceNeedle"></a>
-			
-	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-	        <span class="sr-only">Toggle navigation</span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	        <span class="icon-bar"></span>
-	      </button>
-	      <!--<a class="navbar-brand" href="http://www.206tutoring.com">
-	<img src="../assets/home_page/spaceneedlelogosquaresmall.png" alt="SpaceNeedle">
-	</a>-->
+	<div class="container">
+	 
+	  <div class="row">
+	    <div class="col landing-content">
+	      <a href="../home.php"><img class="top-logo" src=../assets/home_page/spaceneedlelogo.png></a>
 	    </div>
 
-	    <!-- Collect the nav links, forms, and other content for toggling -->
-	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	      <ul class="nav navbar-nav">
-	        <li>
-                        <a href="../test_prep.php">Test and College Prep</a>
-                    </li>
-                    <li>
-                        <a href="../tutoring.html">Tutoring</a>
-                    </li>
-                      <li><a href="/groupclasses.html" class="nav-link">Group Classes</a></li>
-                    <li>
-                        <a href="../music.html">Music Lessons</a>
-                    </li>
+	    <div class="col landing-content text-right">
+	      <a href="../home.php" class="btn btn-blue">Home Page</a>
+	    </div>
+	    
+	    
+	  </div>
+	</div>
 
-                    <li>
-                        <a href="../about.php">About</a>
-                    </li>
-                    <li>
-                        <a href="../instructors.html">Instructors</a>
-                    </li>
-                    <li>
-                        <a href="../payment.php">Payment</a>
-                    </li>
-					
-					
-
-	          </ul>
-	        </li>
-	      </ul>
-		  <ul class="nav navbar-right">
-		                      <li><a href="../contact.php" class="btn btn-orange btn-contact">Contact Us</a></li>
-		                    </ul>
-	     
-	    </div><!-- /.navbar-collapse -->
-	  </div><!-- /.container-fluid -->
-	</nav>
-
-
-
-
-<div class="container">
-	 <h1 class="top-header">SEATTLE'S BEST CALCULUS TUTORS</h1>
-  <div class="row">
+<div class="main-content">
+	<h1 class="top-header">SEATTLE'S BEST CALCULUS TUTORS</h1>
+	
+  	<div class="row">
 	  
-      <div class="landing-content col-sm-4">
+      <div class="landing-content col-md-4 col-sm-6">
         <p>206 Tutoring offers elite, individualized calculus instruction with the<strong> best results</strong> in the Seattle area. Improve your AP score by <strong>one whole point</strong> and your grade by one <strong>full letter.</strong>
 </p>
         <p> Our SAT math scores range from <strong>700 to 800</strong>. We earned<strong> 4’s or 5’s</strong> on the <strong>Calculus AB, Calculus BC</strong>, and <strong>Statistics AP tests</strong>. You can too. <strong>We’ll help you get there.</strong> </p>
 		
         <p> Our <strong>personalized lessons</strong> adapt to each student’s learning style and goals. Meet <strong>in your home</strong> or at the <strong>location of your choice</strong>, across <strong>the entire Seattle area</strong>. </p>
+
+		
+ 	   <p>
+		   
+ 	   <i>	"Over the course of three months Chris taught me many concepts and gave me ways I could teach myself to improve in between lessons. My score went up by 400 points. Thanks so much, Chris!"  - Sofia R., Bellevue</i>
+		
+ 	   </p>
+		
+		<p>Get in touch and start your journey today!</p>
               </div>
 	  
         
-    <div class="landing-content col-sm-4">
-      <?php include '../landingpage-contactform.php'; ?>
+    <div class="landing-content col-md-4 col-sm-6">
+     <?php include '../landingpage-contactform.php'; ?>
 	
     </div>
 
-<div class="landing-content col-sm-4">
-       <img src="../assets/landingpages/cml-pic.jpg" class="img-responsive" alt="Responsive image">
+<div class="landing-content col-md-4 col-sm-6">
+     
 	   <section>
-	   <p>
-	   	"Mr. Morris-Lent recognizes the importance of his role as a “guiding force” in influencing the educational endeavors of his students. He respects parents’ impelling positive impact in the educational voyage of their children … goals not based on dictated tenets and past performances, but toward desired results." <i> - Siri H., Redmond</i>
-		
-	   </p>
+		  <img src="../assets/landingpages/cml-pic.jpg" class="img-responsive" alt="Responsive image"> 
+	   
+	   
    </section>
     </div>
 
@@ -111,7 +81,7 @@
 
 <footer>
   
-      <h1 class="footer-logo">206 <span class="subtext-dark">Tutoring</span> </h1>
+      <a href="../home.php"><h1 class="footer-logo">206 <span class="subtext-dark">Tutoring</span> </h1></a>
 	<div class="footer-text">	
 		 <p> &copy;206 Tutoring &bull; contact@206tutoring.com &bull; (206) 551-7843 </p>
   </div>
@@ -125,5 +95,5 @@
 </html>
 
 
-
+ 
 
